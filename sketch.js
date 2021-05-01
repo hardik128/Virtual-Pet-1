b4e34011afcda690bc46be66a11162141d843002
hardik128 <72390578+hardik128@users.noreply.github.com>
2021-05-01 13:24:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var happyDogImage, dogImage, database, foodStock ,foodS, dog
+
+function preload(){
+	happyDogImage = loadImage("happydog.png");
+  dogImage = loadImage("Dog.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+ 
+  dog = createSprite(400,300,50,50)
+  dog.addImage(dogImage)
+  dog.scale=0.2;
+  database = firebase.database();
+  foodStock = database.ref('food')
+  foodStock.on("value",readStock)
+  console.log(foodS)
+}
+
+
+function draw() {  
+  background(45,65,200)
+  if (keyWentDown(UP_ARROW)){ 
+
+    writeStock(foodS);
+    
+    dog.addImage(happyDogImage)
+  }
+
+  textSize(30)
+  text("food:"+ foodS,200,100)
+  
+  drawSprites();
+  //add styles here
+
+  
+
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1
+  
+  }
+  database.ref('/').update({
+  food:x
+  })
+
+} 
+
