2db4e0c1c76542ea0d4eb0d525d9e29c6a7b9cd5
hardik128 <72390578+hardik128@users.noreply.github.com>
2021-05-02 13:06:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,75 @@
+var happyDogImage, dogImage, database, foodStock ,foodS, dog
+
+function preload(){
+	happyDogImage = loadImage("happydog.png");
+  dogImage = loadImage("Dog.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+ 
+  dog = createSprite(400,300,50,50)
+  dog.addImage(dogImage)
+  dog.scale=0.2;
+  database = firebase.database();
+  foodStock = database.ref('food')
+  foodStock.on("value",readStock)
+  console.log(foodS)
+}
+
+
+function draw() {  
+  background(45,205,200)
+  if (keyWentDown(UP_ARROW)){ 
+
+    writeStock(foodS);
+    
+    dog.addImage(happyDogImage)
+  }
+  if (keyDown("SPACE")){ 
+
+    resetStock();
+    dog.addImage(dogImage)
+    
+  }
+
+  textSize(30)
+  fill(250)
+	stroke(50,50,250)
+	strokeWeight(5)
+  text("food:"+ foodS,350,150)
+  
+  textSize(30)
+  fill(250)
+	stroke(50,50,250)
+	strokeWeight(5)
+  text("Press UP arrow  key to feed and Space key Refill the Stock",5,50)
+  drawSprites();
+ 
+  
+
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1
+  
+  }
+  database.ref('/').update({
+  food:x
+  })
+
+} 
+
+function resetStock(){
+  
+    database.ref('/').update({
+      food:10
+  })
+}
